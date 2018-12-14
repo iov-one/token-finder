0a86068f3863ee29d42fd60fbb11c466c071216b
Simon Warta <simon@kullo.net>
2018-12-14 12:23:12 +0100

    Updates

---
+++ precache-manifest.84f2096cd32ae553ff8ab9bc00b03f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84f4a13bf9b23be933ca",
+    "url": "/token-finder/static/css/main.576f6b41.chunk.css"
+  },
+  {
+    "revision": "84f4a13bf9b23be933ca",
+    "url": "/token-finder/static/js/main.84f4a13b.chunk.js"
+  },
+  {
+    "revision": "0a9d6abe7b55f455ce68",
+    "url": "/token-finder/static/js/1.0a9d6abe.chunk.js"
+  },
+  {
+    "revision": "04a5ef5689c6551616a5",
+    "url": "/token-finder/static/js/runtime~main.04a5ef56.js"
+  },
+  {
+    "revision": "953895af8dfd7b7e0b326e7d97b843e8",
+    "url": "/token-finder/index.html"
+  }
+];
