ce5fbec28305eee095d52f9fc77d00488a91c3ae
Simon Warta <simon@kullo.net>
2018-12-19 11:35:21 +0100

    Use NARROW NO-BREAK SPACE for balances

--- src/bcphelpers.ts
+++ src/bcphelpers.ts
@@ -2,6 +2,10 @@ import { Amount } from "@iov/bcp-types";
 
 import leftPad from "left-pad";
 
+// Unicode NARROW NO-BREAK SPACE
+// https://www.fileformat.info/info/unicode/char/202f/index.htm
+const narrowNoBreakSpace = "\u202F";
+
 export function printAmount(amount: Amount): string {
   const whole = amount.quantity.slice(0, -amount.fractionalDigits) || "0";
   const fractional = leftPad(
@@ -10,5 +14,5 @@ export function printAmount(amount: Amount): string {
     "0"
   );
   const trimmedFractional = fractional.replace(/0+$/, '') || "0";
-  return `${whole}.${trimmedFractional} ${amount.tokenTicker}`;
+  return `${whole}.${trimmedFractional}${narrowNoBreakSpace}${amount.tokenTicker}`;
 }
