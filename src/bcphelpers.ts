f96572b950cde55b33bb2e4f76b73597db19af66
Simon Warta <simon@warta.it>
2019-07-31 06:25:48 +0200

    Avoid using the left-pad package

--- src/bcphelpers.ts
+++ src/bcphelpers.ts
@@ -1,19 +1,13 @@
 import { Amount } from "@iov/bcp";
 import { Slip10RawIndex } from "@iov/crypto";
 
-import leftPad from "left-pad";
-
 // Unicode NARROW NO-BREAK SPACE
 // https://www.fileformat.info/info/unicode/char/202f/index.htm
 const narrowNoBreakSpace = "\u202F";
 
 export function printAmount(amount: Amount): string {
   const whole = amount.quantity.slice(0, -amount.fractionalDigits) || "0";
-  const fractional = leftPad(
-    amount.quantity.slice(-amount.fractionalDigits) || "0",
-    amount.fractionalDigits,
-    "0",
-  );
+  const fractional = (amount.quantity.slice(-amount.fractionalDigits) || "0").padStart(amount.fractionalDigits, "0");
   const trimmedFractional = fractional.replace(/0+$/, "") || "0";
   return `${whole}.${trimmedFractional}${narrowNoBreakSpace}${amount.tokenTicker}`;
 }
