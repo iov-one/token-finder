c5b57899cf29dfbcd09c269d4b5d8eacee45e885
Simon Warta <simon@warta.it>
2019-08-21 20:23:32 +0200

    Updates

---
+++ precache-manifest.99fa2f9b53b9459f3d7020bd602b9a51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8773e67b51519f5c836906bdfd7f6b26",
+    "url": "/token-finder/index.html"
+  },
+  {
+    "revision": "cec1fbd518756fd3494a",
+    "url": "/token-finder/static/css/main.5b431d1d.chunk.css"
+  },
+  {
+    "revision": "9abc031886daeede956e",
+    "url": "/token-finder/static/js/2.05fbf97d.chunk.js"
+  },
+  {
+    "revision": "cec1fbd518756fd3494a",
+    "url": "/token-finder/static/js/main.6d0aa795.chunk.js"
+  },
+  {
+    "revision": "1ac268f59dc37cda5ae9",
+    "url": "/token-finder/static/js/runtime~main.92d21581.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/token-finder/precache-manifest.af7cfc0923a154771062655b56fd549a.js"
+  "/token-finder/precache-manifest.99fa2f9b53b9459f3d7020bd602b9a51.js"
 );
 
 self.addEventListener('message', (event) => {
